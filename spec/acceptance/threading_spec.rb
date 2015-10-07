134ea8804d117440d955812b49536125dc298b78
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-10-07 12:21:06 +0200

    Disable multithread test brittle on Rubinius and JRuby
    Both experience intermittent failures which I can't pinpoint.

--- spec/acceptance/threading_spec.rb
+++ spec/acceptance/threading_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe VCR do
-  context 'when used in a multithreaded environment', :with_monkey_patches => :excon do
+  context 'when used in a multithreaded environment', :with_monkey_patches => :excon, :unless => RUBY_INTERPRETER != :mri do
     def preload_yaml_serializer_to_avoid_circular_require_warning_race_condition
       VCR.cassette_serializers[:yaml]
     end
